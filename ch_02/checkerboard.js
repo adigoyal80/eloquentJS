e68d2c702e0c97d901251d32b71b588c9a897717
Aditya Goyal <aditya.goyal@coatesdigital.com.au>
2018-02-27 18:09:55 +1100

    deleted files of second versionn and added solutions for 3rd chapter 2

---
+++ ch_02/checkerboard.js
@@ -0,0 +1,40 @@
+// function checkerboard(dimension) {
+//     for(let i = 0; i < dimension; i++) {
+//         let line = ''
+//         for ( let x = 0; x < dimension; x++){
+//             if (i % 2 == 0 ){
+//                 if( x % 2 == 0 ){
+//                     line += ' '
+//                 } else {
+//                     line += '#'
+//                 }
+//             } else {
+//                 if( x % 2 == 0 ){
+//                     line += '#'
+//                 } else {
+//                     line += ' '
+//                 }
+//             }
+//         }
+//         console.log(line)
+//     }
+// }
+
+// checkerboard(8)
+
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
---
+++ ch_02/triangle.js
@@ -0,0 +1,9 @@
+function triangle(size) {
+    let output = ''
+    for (let i = 1; i <= size; i++){
+        output += '#'
+        console.log(output)
+    }
+}
+
+triangle(7);
